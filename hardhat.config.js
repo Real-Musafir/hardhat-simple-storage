2ea1256b2eb832b3974fa41681f95c365cf68356
Real-Musafir <alshahadath@gmail.com>
2022-08-06 11:59:38 +0600

    deploy my contract in alcamy in rinkeby network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const RINKEBY_RPC_RUL = process.env.RINKEBY_RPC_RUL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/HNHfuJ4Brhv9JB0YG8V-PDJ-RryCDBlv",
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+    },
+  },
+  allowUnlimitedContractSize: true,
   solidity: "0.8.9",
 };
